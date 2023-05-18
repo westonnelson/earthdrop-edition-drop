5cb44b151d584454b1a0e761d5b19c6f1f2aef7c
Weston Nelson <dogooddowell@protonmail.com>
2023-05-18 01:19:34 -0500

    Update _app.tsx
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <Head>
-        <title>Edition Drop Minting</title>
+        <title>Earth Drop Edition Mint</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Mint page for Edition Drop NFT"
+          content="Mint Page for NFTEarth Earth Drop"
         />
         <meta
           name="keywords"
